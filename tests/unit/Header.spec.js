231fb031fb81505ed1eef91dd793d4be0936c873
Jaanus <jaanusraudsik@hotmail.com>
2019-12-12 23:41:38 +0200

    another minor fix

--- tests/unit/Header.spec.js
+++ tests/unit/Header.spec.js
@@ -12,21 +12,20 @@ describe('Header is rendered correctly', () => {
 
     it('Renders the correct year', () => {
         let today = new Date().getFullYear();
-        let year = today < 10 ? `0${today}` : today;
-        expect(wrapper.html()).toContain(year)
+        expect(wrapper.html()).toContain(today)
     });
 
     it('Renders the correct month', () => {
         let today = new Date().getMonth();
         let months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October',	'November', 'December'];
-        let month = months[today] < 10 ? `0${months[today]}` : months[today];
+        let month = months[today];
         expect(wrapper.html()).toContain(month.toUpperCase().slice(0,3))
     });
 
     it('Renders the correct weekdays', () => {
         let today = new Date().getDay();
         let weekdays = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
-        let weekday = weekdays[today] < 10 ? `0${weekdays[today]}` : weekdays[today];
+        let weekday = weekdays[today];
         expect(wrapper.html()).toContain(weekday.toUpperCase())
     });
 });
