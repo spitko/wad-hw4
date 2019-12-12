31f142473dc1168e6ecd95482491c32bd4681aa2
Jaanus <jaanusraudsik@hotmail.com>
2019-12-12 23:36:57 +0200

    few minor fixes

--- tests/unit/Footer.spec.js
+++ tests/unit/Footer.spec.js
@@ -5,12 +5,8 @@ import Vue from "vue";
 describe('Footer component', () => {
     const wrapper = mount(Footer);
 
-    it('Clicking add button sets "open" property in component', async () => {
-        expect(wrapper.find('div > div > input').exists()).toBe(false);
-        expect(wrapper.find('div > div > span').isVisible()).toBe(true);
+    it('Clicking add button sets "open" property in component', () => {
         wrapper.find('span').trigger('click');
-        await Vue.nextTick();
-        expect(wrapper.find('div > div > input').isVisible()).toBe(true);
-        expect(wrapper.find('div > div > span').exists()).toBe(false);
+        expect(wrapper.vm.$data.open);
     });
 });
--- tests/unit/List.spec.js
+++ tests/unit/List.spec.js
@@ -1,18 +1,26 @@
 import List from "../../src/components/List";
 import {mount} from "@vue/test-utils";
+import Item from "../../src/models/Item";
 
 describe('List component', () => {
-    const testData = [];
 
-    const wrapper = mount(List, {
-        propsData: {
-            list: testData
-        }
-    });
+    const wrapper = mount(List);
 
     it('check if "Add your first Todo task" is displayed if List is empty', () => {
         expect(wrapper.html()).toContain('Add your first Todo task')
     });
+
+    it('Check if item is updated correctly when the item in the list is marked as done', async() => {
+        wrapper.props().list.push(new Item("New item"));
+        await wrapper.vm.$nextTick();
+
+        expect(wrapper.props().list[0].done).toBe(false);
+
+        wrapper.find('.list-item > div > span').trigger('click');
+        await wrapper.vm.$nextTick();
+
+        expect(wrapper.props().list[0].done).toBe(true);
+    })
 
 });
 
